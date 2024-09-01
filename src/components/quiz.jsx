e824023b33ca5c622b4a1ce1a81f175b712cb34e
beteab548 <beteabbaynessagne548@gmail.com>
2024-08-31 22:30:34 -0700

    updated quiz component

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Questions from "../../questions.js";
+import quizComplete from "../assets/quiz-complete.png";
 export default function Quiz() {
   const [userAnswer, setUserAnswer] = useState([]);
   const activeQuestionIndex = userAnswer.length;
@@ -8,12 +9,24 @@ export default function Quiz() {
       return [...prevValue, answer];
     });
   }
+  const quizIsComplete = Questions.length === userAnswer.length;
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizComplete} alt="quiz complete image" />
+        <h2>quiz completed!</h2>
+      </div>
+    );
+  }
+  const suffledAnswers = Questions[activeQuestionIndex].answers.sort(() => {
+    return Math.random() - 0.5;
+  });
   return (
     <div id="quiz">
       <div id="questions">
         <h2>{Questions[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {Questions[activeQuestionIndex].answers.map((answers) => {
+          {suffledAnswers.map((answers) => {
             return (
               <li key={answers} className="answer">
                 <button
