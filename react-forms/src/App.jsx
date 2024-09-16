cf4e23fdc6d0bb97c2155336a2d1b5d6e0850848
beteab548 <beteabbaynessagne548@gmail.com>
2024-09-16 12:45:53 -0700

    updated sinup

--- react-forms/src/App.jsx
+++ react-forms/src/App.jsx
@@ -1,15 +1,14 @@
-import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
+import Header from "./components/Header.jsx";
+import Signup from "./components/signup.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
 }
-
 export default App;
