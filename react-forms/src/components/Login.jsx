734308c36144b31962de01004e74595cefd208d8
beteab548 <beteabbaynessagne548@gmail.com>
2024-09-16 11:47:37 -0700

    updated login

--- react-forms/src/components/Login.jsx
+++ react-forms/src/components/Login.jsx
@@ -1,20 +1,46 @@
+import { useState } from "react";
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+  function onSubmit(event) {
+    event.preventDefault();
+    console.log(enteredValues);
+  }
+  function handleEnteredValues(idetifier, value) {
+    setEnteredValues((prevValue) => {
+      return { ...prevValue, [idetifier]: value };
+    });
+  }
+  console.log(enteredValues);
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2>Login</h2>
-
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(e) => {
+              handleEnteredValues("email", e.target.value);
+            }}
+          />
         </div>
-
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => {
+              handleEnteredValues("password", e.target.value);
+            }}
+          />
         </div>
       </div>
-
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
         <button className="button">Login</button>
