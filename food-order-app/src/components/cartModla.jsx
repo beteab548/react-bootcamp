8e87cf91041e9f879e486fc52787e545e0ebed6a
beteab548 <beteabbaynessagne548@gmail.com>
2024-09-23 20:58:23 -0700

    modal updated

--- food-order-app/src/components/cartModla.jsx
+++ food-order-app/src/components/cartModla.jsx
@@ -1,1 +1,10 @@
-export default function cartModal(){}
+export default function CartModal({ isOpen }) {
+  console.log(isOpen);
+  return (
+    <dialog open={isOpen} >
+      <p>cart is opened!</p>
+      <p>your items!</p>
+      <button ></button>
+    </dialog>
+  );
+}
--- food-order-app/src/components/header.jsx
+++ food-order-app/src/components/header.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import CartModal from "./cartModla";
 
 export default function Header() {
+  const [cartBtnIsClicked, setCartBtnClicked] = useState(false);
+  function cartBtnClicked() {
+    setCartBtnClicked((prevValue) => {
+      return !prevValue;
+    });
+  }
   return (
     <div>
-      <img src='' alt="" />
+      <img src="" alt="" />
       <p>REACTFOOD</p>
+      <button onClick={cartBtnClicked}>cart{1}</button>
+      <CartModal isOpen={cartBtnIsClicked}/>
     </div>
   );
 }
