e088528f76b9141c4c38521299b8820e865414f0
beteab548 <beteabbaynessagne548@gmail.com>
2024-09-30 15:58:23 -0700

    added conected a component to a store

--- redux-practice/src/components/Counter.jsx
+++ redux-practice/src/components/Counter.jsx
@@ -1,16 +1,24 @@
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 const toggleCounterHandler = () => {};
 
 export default function Counter() {
+  const dispache = useDispatch();
   const latestState = useSelector((state) => {
     return state.counter;
   });
-  console.log(latestState);
+  function incrementFUnction() {
+    dispache({ type: "increment" });
+  }
+  function decrementDunction() {
+    dispache({ type: "decrement" });
+  }
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{latestState}</div>
+      <button onClick={incrementFUnction}>increment Counter</button>
+      <button onClick={decrementDunction}>decrement Counter</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
