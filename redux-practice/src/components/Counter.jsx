5532d3cc587a34bba64331e50c81c807228459e4
beteab548 <beteabbaynessagne548@gmail.com>
2024-10-01 17:30:48 -0700

    added redux toolkits to create a store

--- redux-practice/src/components/Counter.jsx
+++ redux-practice/src/components/Counter.jsx
@@ -1,24 +1,30 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
-const toggleCounterHandler = () => {};
-
+import { counterActions } from "../../store/index.js";
 export default function Counter() {
-  const dispache = useDispatch();
+  const dispatch = useDispatch();
   const latestState = useSelector((state) => {
     return state.counter;
   });
-  function incrementFUnction() {
-    dispache({ type: "increment" });
+  const showCounter = useSelector((state) => {
+    return state;
+  });
+  function incrementFunction() {
+    dispatch(counterActions.increment);
   }
-  function decrementDunction() {
-    dispache({ type: "decrement" });
+  function decrementFunction() {
+    dispatch(counterActions.decrement);
   }
+  function toggleCounterHandler() {
+    dispatch(counterActions.toggleCounter);
+  }
+  console.log(latestState);
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{latestState}</div>
-      <button onClick={incrementFUnction}>increment Counter</button>
-      <button onClick={decrementDunction}>decrement Counter</button>
+      {showCounter && <div className={classes.value}>{latestState}</div>}
+      <button onClick={incrementFunction}>increment Counter</button>
+      <button onClick={decrementFunction}>decrement Counter</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- redux-practice/store/index.js
+++ redux-practice/store/index.js
@@ -1,12 +1,23 @@
-import { createStore } from "redux";
-const ReduxFunction = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1 };
-  }
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
-  }
-  return  state ;
-};
-const store = createStore(ReduxFunction);
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+const intialState = { counter: 0, showCounter: true };
+const counterSlice = createSlice({
+  name: "counter",
+  intialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+const store = configureStore({ reducer: counterSlice.reducer });
+export const counterActions = counterSlice.actions;
 export default store;
