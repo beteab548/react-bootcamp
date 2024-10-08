1e4a92fc90c0ba35d426ed289bd53e453f592544
beteab548 <beteabbaynessagne548@gmail.com>
2024-10-07 23:55:02 -0700

    added neccessary components to routing practice

--- routing-practice/src/App.js
+++ routing-practice/src/App.js
@@ -19,7 +19,6 @@
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 function App() {
   return <div></div>;
 }
