a5713f2ef38abe852775696d4a5f503daba77c9f
beteab548 <beteabbaynessagne548@gmail.com>
2024-10-15 10:30:27 -0700

    setting up files to start authentication practice

--- react-authentication/src/pages/Authentication.js
+++ react-authentication/src/pages/Authentication.js
@@ -1,7 +1,28 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+  const data = await request.formData();
+  const response = await fetch("http://localhost:8080" + mode, {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "unsupported mode" }, { status: 500 });
+  }
+  if (!response.ok) {
+    throw json({ message: "response error! " }, { status: 500 });
+  }
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+  return redirect("/");
+}
